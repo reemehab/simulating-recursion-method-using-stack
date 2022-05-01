02892f2a07a8059e99a6e0d245664738fc7c5435
reemehab <60987184+reemehab@users.noreply.github.com>
2022-05-01 05:22:45 +0200

    sixth version

--- main.cpp
+++ main.cpp
@@ -7,117 +7,87 @@ int F(int n)
         return 1;
     int a=n+F(n-1);
     int b=n*F(n/2);
-   // int c=n-2-(a+b)%2;
-    //int d=F(c);
-    //cout<<"a "<<a<<" b "<<b<<endl;
-    return a+b;
+    int c=n-2-(a+b)%2;
+    int d=F(c);
+    return a+b+d;
 }
 
 int F_iterative(int n)
-{
+{   int original_n=n;
     int address = 10; // Entry point for each each "call"
-    int temp_B = n;
-    int temp_A=n;
-    int n2=n;
-    stack<int> s ;
-    int a=0 , b=0;
-    //computing A
+    stack<int> s;
+    int temp=n;
+    int a=1, b=1 , c , d;
+    int *arr;
+    arr= new int [n]; // i will store in it the value of the Functions mo2akatan
    s.push(30); // Initial return address
-    s.push(temp_A);
+   s.push(n);
+   int index=0;
     while(!s.empty())
     {
         switch(address)
         {
-            case 10: //if call type equals 3
+            case 10:
             {
-                n2 = s.top();
+                n = s.top();
                 s.pop();
-                if(n2 <=1)
+                if(n <=1)
                 { // The base case
                     address = s.top();
                     s.pop();
-                    s.push(1);
+                    arr[index]=1;
                 }
                 else
                 {
-                    s.push(n2);
-                    s.push(20); // Where should I return?
-                    s.push(n2-1);
-                    address = 10; // Make another "call"
-                }
-                break;
-            }
-            case 20:  // if call type equals compute
-            { // Compute and return
-                temp_A = s.top();
-                s.pop();
-                n2 = s.top();
-                s.pop();
-                address = s.top();
-                s.pop();
-                temp_A = n2 + temp_A;
-                s.push(temp_A);
-                break;
-            }
-            case 30:  // if call type equals call
-                // The final return value
-                temp_A = s.top();
-                s.pop();
-
-        }}
-        a =a+ temp_A;
-
-    address=10;
-    s.push(30); // Initial return address
-    s.push(temp_B);
-    while(!s.empty()) {
-        switch (address) {
-            case 10: //if call type equals 3
-            {
-                n = s.top();
-                s.pop();
-                if (n <= 1) { // The base case
-                    address = s.top();
-                    s.pop();
-                    s.push(1);
-                } else {
                     s.push(n);
-                    s.push(20); // Where should I return?
-                    s.push(n / 2);
+                    s.push(20); // Address to point to it after finishing case 10
+                    s.push(n-1);
                     address = 10; // Make another "call"
                 }
                 break;
             }
-            case 20:  // if call type equals compute
+            case 20:  // address 20
             { // Compute and return
-                temp_B = s.top();
+                temp = s.top();
                 s.pop();
-                n = s.top();
-                s.pop();
+                a=arr[temp-2]+temp;
+                b=temp*arr[(temp/2)-1];
+                c=temp-2-(a+b)%2;
+                if(c<0){c=0;}
+                else {c--;}
+                d=arr[c];
+                index++;
+                arr[index]=a+b+d;
+               // cout<<arr[index]<<endl;
                 address = s.top();
                 s.pop();
-                temp_B = n * temp_B;
-                s.push(temp_B);
                 break;
             }
-            case 30:  // if call type equals call
-                // The final return value
-                temp_B = s.top();
+            case 30:
+                // The final return value to break the loop
+                temp = s.top();
                 s.pop();
         }
     }
-        b= b+temp_B;
-
-  //  cout<<"a "<<a<<" b "<<b<<endl;
-    return a+b;
+    return arr[original_n-1];
 }
 int main() {
 
     cout<<F(2)<<endl;
     cout<<F_iterative(2)<<endl ;
+    cout<<F(3)<<endl;
+    cout<<F_iterative(3)<<endl ;
     cout<<F(4)<<endl;
     cout<<F_iterative(4)<<endl ;
-    cout<<F(5)<<endl;
-    cout<<F_iterative(5)<<endl ;
+    cout<<F(6)<<endl;
+    cout<<F_iterative(6)<<endl ;
+    cout<<F(7)<<endl;
+    cout<<F_iterative(7)<<endl ;
+    cout<<F(8)<<endl;
+    cout<<F_iterative(8)<<endl ;
+    cout<<F(9)<<endl;
+    cout<<F_iterative(9)<<endl ;
+    cout<<F(10)<<endl;
+    cout<<F_iterative(10)<<endl ;
     return 0;
 }
