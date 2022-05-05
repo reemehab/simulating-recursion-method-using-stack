1e3998562cc9b1f91f128ab5c244a28a70444508
reemehab <60987184+reemehab@users.noreply.github.com>
2022-05-05 16:34:18 +0200

    Final Final with comments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include<stack>
-
 using namespace std;
-
-int F(int n) {
+int F(int n)
+{
     if (n <= 1) return 1;
     int a = n + F(n - 1);
     int b = n * F(n / 2);
@@ -12,88 +11,92 @@ int F(int n) {
     return a + b + d;
 }
 
-struct SnapShotStruct{
-    int a , b , c,n,stage;
+// a struct called snapshot to hold a , b, c, n , address of the next instruction
+struct SnapShot
+{
+    int a, b, c, n, address;
 };
 
 int F_iterative(int n)
-{   int retVal = 0;  // initialize with default returning value
-    stack<SnapShotStruct> snapshotStack;
-    SnapShotStruct currentSnapshot;
-    int  d;
-    currentSnapshot.n=n;
-    currentSnapshot.a=0;
-    currentSnapshot.b=0;
-    currentSnapshot.c=0;
-    currentSnapshot.stage=10;
-    snapshotStack.push(currentSnapshot);
+{
+    int retVal = 0;  // initialize with default returning value
+    stack<SnapShot> snapshotStack; // intializing stack of type SnapShot
+    SnapShot currentSnapshot; // creating an instance of type SnapShot to be pushed in the Stack as the first call
+    currentSnapshot.n=n;     // intializig the instance's n with the n coming from the actual parammeter
+    currentSnapshot.a=0;    // intializing the instance's a with 0 to be calculated later
+    currentSnapshot.b=0;   // intializing the instance's b with 0 to be calculated later
+    currentSnapshot.c=0;   // intializing the instance's c with 0 to be calculated later
+    currentSnapshot.address=10;  // intializing the instance's address with 10 to make this object enter case 10 in the switch case down
+    snapshotStack.push(currentSnapshot);  //push this instance in the stack
     while(!snapshotStack.empty())
-    {   currentSnapshot=snapshotStack.top();
-        snapshotStack.pop();
-        switch(currentSnapshot.stage)
+    {
+        currentSnapshot=snapshotStack.top(); // holds the top of the stack in current snap shot variable
+        snapshotStack.pop(); //delets the top of the stack
+        //each address in the switch case points to a call to be done
+        switch(currentSnapshot.address)
         {
             case 10:
-            {
-                if(currentSnapshot.n <=1)
-                { // The base case
+                //we are pushing instances from instance.n=n to  instance.n=1 at n=n-1 until base case is reached
+                {
+                 if(currentSnapshot.n<=1)
+                {
                     retVal=1;
                     continue;
-
                 }
                 else
                 {
-                    currentSnapshot.stage = 20;
+                    currentSnapshot.address = 20; //chanding the adress of the instance that is pushed in the stack with adrress 10  to be 20 to be ready to enter new call
                     snapshotStack.push(currentSnapshot);
-                    SnapShotStruct newSnapshot;
-                    newSnapshot.n= currentSnapshot.n-1;
-                    newSnapshot.a=0;
-                    newSnapshot.b=0;
-                    newSnapshot.c=0;
-                    newSnapshot.stage=10;
+                    SnapShot newSnapshot;
+                    newSnapshot.n= currentSnapshot.n-1; // pushing a new instance of n = previous n -1
+                    newSnapshot.a=0; //intializing the instance's a with 0 to be calculated later in case 20
+                    newSnapshot.b=0; //intializing the instance's b with 0 to be calculated later in  case 30
+                    newSnapshot.c=0; //intializing the instance's c with 0 to be calculated later  in case 30
+                    newSnapshot.address=10; //intializing the instance's address with 10 to make this object enter case 10 again
                     snapshotStack.push(newSnapshot);
                 }
                 break;
             }
 
             case 20:
-                //computing a
+                // Compute a
             {
-                currentSnapshot.a=currentSnapshot.n+retVal;
-                currentSnapshot.stage=30;
-                snapshotStack.push(currentSnapshot);
-                currentSnapshot.stage=10;
-                currentSnapshot.n=(currentSnapshot.n)/2;
-                snapshotStack.push(currentSnapshot);
+                currentSnapshot.a=currentSnapshot.n+retVal;  //a=N+ previous retVal
+                currentSnapshot.address=30; // change the adress of this instance to indicate that its next stage is 30 to compute b , c
+                snapshotStack.push(currentSnapshot); //push this instance in the stack
+                currentSnapshot.address=10; // change the address of this call to 10
+                currentSnapshot.n=currentSnapshot.n/2; // with n=n/2 to be prepared to enter case 30 to compute its b
+                snapshotStack.push(currentSnapshot); // push to the stack
                 break;
             }
             case 30:
-                //compute b
+                // Compute b and c
             {
-                currentSnapshot.b=currentSnapshot.n*retVal;
-                currentSnapshot.c=currentSnapshot.n-2-(currentSnapshot.a+currentSnapshot.b)%2;
-                currentSnapshot.stage=40;
-                snapshotStack.push(currentSnapshot);
-                SnapShotStruct newCall;
-                newCall.n = currentSnapshot.c;
-                newCall.stage = 10;
-                snapshotStack.push(newCall);
-
+                currentSnapshot.b=currentSnapshot.n*retVal; //b-n* result of previous N/2
+                currentSnapshot.c=currentSnapshot.n-2-(currentSnapshot.a+currentSnapshot.b)%2; // c= n-2 -(a+b)%2
+                currentSnapshot.address=40; // changing the adddress of the call to be wo to compute d
+                snapshotStack.push(currentSnapshot); //pushing this call to stack
+                SnapShot newCall; //new instance to be prepared to compute the d
+                newCall.n = currentSnapshot.c; // which has its n = to c
+                newCall.address = 10; // the adress of this new call is to be 10 to start computing a, b , c ,d from the begining for d=f(c) which is equals to reVal
+                snapshotStack.push(newCall); // pushing the new call to the stack
             }
                 break;
-            case 40:// compute d
-            {
-
-                retVal=currentSnapshot.a+currentSnapshot.b+retVal;
+            case 40:
+                // Compute d
+            {   int d=retVal; // d is equal to previous reVal from newCall in case 30
+                retVal=currentSnapshot.a+currentSnapshot.b+d;
                 break;
             }
-
         }
     }
     return retVal;
 }
-int main() {
+int main()
+{
 
-    for (int i = 1; i <=20 ; i++) {
+    for (int i = 1; i <=20 ; i++)
+    {
         cout<<F(i)<<endl;
         cout<<F_iterative(i)<<endl ;
     }
